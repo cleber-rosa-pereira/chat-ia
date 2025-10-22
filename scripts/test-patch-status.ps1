9c64c03ee9580fd0b16026ff17fbcee118108fb4
Cleber da Rosa Pereira <laudos.digitacaorapida@gmail.com>
2025-10-22 16:44:38 -0300

    wip: checkpoint antes de trocar de projeto

---
+++ scripts/test-patch-status.ps1
@@ -0,0 +1,30 @@
+﻿param(
+  [Parameter(Mandatory=$true)] [string]$Id
+)
+
+Write-Host "== Teste: 400 invalid_status =="
+$bad = @{ status = "banana" } | ConvertTo-Json -Compress
+try {
+  Invoke-RestMethod -Method Patch -Uri "http://localhost:3333/appointments/$Id/status" -ContentType "application/json" -Body $bad
+} catch {
+  Write-Host ">> HTTP:" $_.Exception.Response.StatusCode.value__
+  if ($_.ErrorDetails.Message) { Write-Host ">> Body:" $_.ErrorDetails.Message }
+  else {
+    $sr = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+    $body = $sr.ReadToEnd(); $sr.Close(); Write-Host ">> Body:" $body
+  }
+}
+
+Write-Host "`n== Teste: 404 not_found =="
+$fakeID = [guid]::NewGuid().ToString()
+$ok = @{ status = "scheduled" } | ConvertTo-Json -Compress
+try {
+  Invoke-RestMethod -Method Patch -Uri "http://localhost:3333/appointments/$fakeID/status" -ContentType "application/json" -Body $ok
+} catch {
+  Write-Host ">> HTTP:" $_.Exception.Response.StatusCode.value__
+  if ($_.ErrorDetails.Message) { Write-Host ">> Body:" $_.ErrorDetails.Message }
+  else {
+    $sr = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+    $body = $sr.ReadToEnd(); $sr.Close(); Write-Host ">> Body:" $body
+  }
+}
